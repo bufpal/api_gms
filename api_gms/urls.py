54ca755a410341888f82862dd5cc67a05d6eeaa8
Ben Woo <bufpal@gmail.com>
2018-08-10 14:48:46 +0900

    added media_url, media_root, set url to serve media in dev

--- api_gms/urls.py
+++ api_gms/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
@@ -16,3 +18,5 @@ urlpatterns = [
     re_path(r'^rest-auth/', include('rest_auth.urls')),
     re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
