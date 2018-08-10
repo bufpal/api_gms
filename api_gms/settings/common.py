253143433de778a46589fb48dad9585fe70a0940
Ben Woo <bufpal@gmail.com>
2018-08-10 18:05:29 +0900

    set root for Staticfiles, set DIRS for template

--- api_gms/settings/common.py
+++ api_gms/settings/common.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'api_gms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'api_gms', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
