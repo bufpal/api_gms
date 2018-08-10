7b3cdf32db051ae0b879ee693aa7d425f09c1774
Ben Woo <bufpal@gmail.com>
2018-08-11 08:47:15 +0900

    set DB: azure-storage, postgresql

--- api_gms/settings/prod.py
+++ api_gms/settings/prod.py
@@ -1,1 +1,17 @@
 from .common import *
+
+STATICFILES_STORAGE = 'api_gms.storages.StaticAzureStorage'
+DEFAULT_FILE_STORAGE = 'api_gms.storages.MediaAzureStorage'
+
+AZURE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    },
+}
